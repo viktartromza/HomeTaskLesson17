a9121b26e07ab04bd809ac45da0cffb7c3605b6e
ViktarTromza <viktar.tromza@gmail.com>
2022-11-28 01:21:50 +0300

    Initial

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+    //Написать программу для проверки на валидность введенного ip адреса. Пусть ip адрес задается с консоли.
+    //Программа должна проверять валидность введенного ip адреса с помощью регулярного выражения и выводить результат проверки на экран.
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         String ip = scanner.nextLine();
-        Pattern pattern = Pattern.compile("([0-2]?[0-9]{1,2}[.]){4}");
+        Pattern pattern = Pattern.compile("([01]?\\d\\d?.|2[0-4]\\d.|25[0-5].){4}");
         Matcher matcher = pattern.matcher(ip);
-        boolean ipCorrect = matcher.matches();
-        if (ipCorrect) {
-            String[] domain = ip.split(".");
+        System.out.println(matcher.matches());
 
-            for (int i = 0; i < domain.length; ++i) {
-                if (Integer.parseInt(domain[i]) > 255) {
-                    ipCorrect = false;
-                    break;
-                }
-            }
-        }
-        System.out.println(ipCorrect);
+//Программа на вход получает путь к файлу (формат тхт). Файл содержит произвольный текст.
+// В этом тексте может быть номер документа(один или несколько), емейл и номер телефона.
+// Номер документа в формате: xxxx-xxxx-xx, где x - это любая цифра; номер телефона в формате: +(ХХ)ХХХХХХХ.
+// Документ может содержать не всю информацию, т.е. например, может не содержать номер телефона, или другое.
+// Необходимо извлечь информацию. Извлеченную информацию необходимо сохранить в следующую структуру данных:
+// Map, где ключ типа String – это что сохраняем(email, document number, phone number), значение типа String с информацией.
+// В конце вывести все найденные элементы Map.
         FileReader fileReader = new FileReader("D://Task.txt");
         StringBuilder builder = new StringBuilder();
         int c;
@@ -33,7 +32,7 @@ public class Main {
         fileReader.close();
         String text = builder.toString();
         Pattern patternNumberOfDoc = Pattern.compile("([0-9]{4}-){2}[0-9]{2}");
-        Pattern patternTelephone = Pattern.compile("[+][0-9]{3}[(][0-9]{2}[)][0-9]{7}");
+        Pattern patternTelephone = Pattern.compile("[+][0-9]{3}[(][0-9]{2}[)][0-9]{7}");// поменял формат на +ХХХ(ХХ)ХХХХХХХ
         Pattern patternEmail = Pattern.compile("\\w+@\\w+[.]\\w{2,3}");
         Matcher matcherNumberOfDoc = patternNumberOfDoc.matcher(text);
         Matcher matcherTelephone = patternTelephone.matcher(text);
